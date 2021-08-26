f7687bb8e69fef6db8d021b9f61de2e74b752cea
Andrew Hardy <aha@cluedin.com>
2021-08-26 11:39:59 +1000

    Updated icon

--- src/Connector.SqlServer/AzureServiceBusConstants.cs
+++ src/Connector.SqlServer/AzureServiceBusConstants.cs
@@ -29,7 +29,7 @@ namespace CluedIn.Connector.AzureServiceBus
 
         public static IList<string> ServiceType = new List<string> { "Connector" };
         public static IList<string> Aliases = new List<string> { "AzureEventConnector" };
-        public const string IconResourceName = "Resources.azureeventhub.png";
+        public const string IconResourceName = "Resources.service-bus.svg";
         public const string Instructions = "Provide authentication instructions here, if applicable";
         public const IntegrationType Type = IntegrationType.Connector;
         public const string Category = "Connectivity";
