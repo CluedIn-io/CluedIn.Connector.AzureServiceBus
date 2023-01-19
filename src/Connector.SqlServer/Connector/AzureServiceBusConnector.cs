5b2c42358511548b80bed2b15f0898e02fc95842
Andrew Hardy <aha@cluedin.com>
2023-01-19 23:50:10 +1000

    .

--- src/Connector.SqlServer/Connector/AzureServiceBusConnector.cs
+++ src/Connector.SqlServer/Connector/AzureServiceBusConnector.cs
@@ -210,8 +210,8 @@ namespace CluedIn.Connector.AzureServiceBus.Connector
                 {
                     if (value is (ServiceBusClient client, ServiceBusSender sender))
                     {
-                        sender.DisposeAsync().GetAwaiter().GetResult();
-                        client.DisposeAsync().GetAwaiter().GetResult();
+                        sender.DisposeAsync();
+                        client.DisposeAsync();
                     }
                 });
 
