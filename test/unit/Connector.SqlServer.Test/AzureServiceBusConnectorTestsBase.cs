f0e044b48739b740bf23121bd4ea0dc4bf2fe9c1
andrew-hardy <aha@cluedin.com>
2023-04-26 06:26:21 +0000

    chore: Update for latest core changes (#22)

--- test/unit/Connector.SqlServer.Test/AzureServiceBusConnectorTestsBase.cs
+++ test/unit/Connector.SqlServer.Test/AzureServiceBusConnectorTestsBase.cs
@@ -1,6 +1,5 @@
 using CluedIn.Connector.AzureServiceBus.Connector;
 using CluedIn.Core.Caching;
-using CluedIn.Core.DataStore;
 using Microsoft.Extensions.Logging;
 using Moq;
 
@@ -9,14 +8,13 @@ namespace CluedIn.Connector.AzureServiceBus.Unit.Tests
     public class AzureServiceBusConnectorTestsBase
     {
         protected readonly AzureServiceBusConnector Sut;
-        protected readonly Mock<IConfigurationRepository> Repo = new Mock<IConfigurationRepository>();
         protected readonly Mock<ILogger<AzureServiceBusConnector>> Logger = new Mock<ILogger<AzureServiceBusConnector>>();
         protected readonly Mock<IApplicationCache> Cache = new Mock<IApplicationCache>();
         protected readonly TestContext Context = new TestContext();
 
         public AzureServiceBusConnectorTestsBase()
         {
-            Sut = new AzureServiceBusConnector(Repo.Object, Logger.Object, Cache.Object);
+            Sut = new AzureServiceBusConnector(Logger.Object, Cache.Object);
         }
     }
 }
