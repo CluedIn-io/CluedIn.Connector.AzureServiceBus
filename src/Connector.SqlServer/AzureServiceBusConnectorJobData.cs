0df0af23a248d26509aa9a16659a0a999e1c74a9
andrew-hardy <aha@cluedin.com>
2023-08-24 15:32:35 +1000

    feat: Implement buffering and connection reuse (#32) (#33)
    Work Item ID: AB#22928

--- src/Connector.SqlServer/AzureServiceBusConnectorJobData.cs
+++ src/Connector.SqlServer/AzureServiceBusConnectorJobData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CluedIn.Core.Crawling;
 
@@ -5,6 +6,10 @@ namespace CluedIn.Connector.AzureServiceBus
 {
     public class AzureServiceBusConnectorJobData : CrawlJobData
     {
+        public string ConnectionString { get; set; }
+
+        public string Name { get; set; }
+
         public AzureServiceBusConnectorJobData(IDictionary<string, object> configuration)
         {
             if (configuration == null)
@@ -16,6 +21,36 @@ namespace CluedIn.Connector.AzureServiceBus
             Name = GetValue<string>(configuration, AzureServiceBusConstants.KeyName.Name);
         }
 
+        protected bool Equals(AzureServiceBusConnectorJobData other)
+        {
+            return ConnectionString == other.ConnectionString && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((AzureServiceBusConnectorJobData)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ConnectionString, Name);
+        }
+
         public IDictionary<string, object> ToDictionary()
         {
             return new Dictionary<string, object> {
@@ -23,9 +58,5 @@ namespace CluedIn.Connector.AzureServiceBus
                 { AzureServiceBusConstants.KeyName.Name, Name }
             };
         }
-
-        public string ConnectionString { get; set; }
-
-        public string Name { get; set; }
     }
 }
